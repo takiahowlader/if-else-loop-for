8d60b7df19133e6aaff6536ffc6037148a8fde4c
takiahowlader <takiahowlader4466@gmail.com>
2024-11-30 14:29:55 +0600

    first commit

---
+++ if-else.js
@@ -0,0 +1,9 @@
+var day = 20;
+var night = 16;
+
+if(day>22){
+    console.log("good day");
+}else if(night<22){
+    console.log("nice night");
+    
+}
---
+++ loop-for.js
@@ -0,0 +1,13 @@
+var numbers = [10,15,20,25,30,]
+for(var i = 0; i < numbers.length;i++){
+    var elements = numbers[i]
+    console.log(elements);
+    
+}
+
+for(var Book = 0; Book < 15; Book++){
+    console.log("Bangla, English, Math,Islamic history");
+    console.log(Book);
+    
+    
+}
